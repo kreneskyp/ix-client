af4c992383fde04b4621a14e63a725d6e8f735c4
kreneskyp <kreneskyp@users.noreply.github.com>
2023-09-10 22:59:02 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="kreneskyp"
+original_name="ix_client"
+original_urlname="ix-client"
+original_description="Awesome ix_client created by kreneskyp"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ix_client $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ ix_client/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for ix_client."""
+
+from ix_client.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
